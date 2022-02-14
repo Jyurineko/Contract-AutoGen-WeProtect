b8cbe7a24f6918afb59485b4c652d79f7ab70da6
Jyurineko <ilyetn@gmail.com>
2022-02-14 16:48:16 +0800

    add gen-ted Contract & menu
    增加了Menu，包含个法、商法的菜单
    代码更加简洁

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from docx import Document
 import os
 
+
 # def print_hi(name):
 #     # 在下面的代码行中使用断点来调试脚本。
 #     print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
@@ -14,7 +15,7 @@ import os
 # https://www.codegrepper.com/code-examples/python/python+tkinter+text+get
 
 def main():
-
+    # exec 动态函数调用 勿删
     def focus_next_window(event):
         event.widget.tk_focusNext().focus()
         return "break"
@@ -98,62 +99,100 @@ def main():
                                 old_laws: new_laws, old_currency: new_currency, old_effect: new_effect,
                                 old_notice: new_notice, old_compen: new_compen}))
 
-
-        #  todo 客户单号作为后缀
-        #  filename = 'Independent_Contractor_Agreement-' + 客户单号 + '.docx'
-        filename = 'Independent_Contractor_Agreement_UserID' + doc_name + '.docx'
-        filepath = './'
+        filename = 'UserID_' + doc_name + 'Independent_Contractor_Agreement.docx'
+        filepath = './Generated-Contracts/'
         doc.save(filepath + filename)
 
         #  加密word，密码是user ID
 
         #  打开word
-        os.system('start' + filepath)
+        os.system('start' + filepath + filename)
+
+    def menu_New():
+        textbox_name.delete("1.0", "end-1c")
+        textbox_uid.delete("1.0", "end-1c")
+        textbox_date.delete("1.0", "end-1c")
+        textbox_addr.delete("1.0", "end-1c")
+        textbox_curr.delete("1.0", "end-1c")
+        textbox_city.delete("1.0", "end-1c")
+        textbox_laws.delete("1.0", "end-1c")
+        textbox_effe.delete("1.0", "end-1c")
+        textbox_noti.delete("1.0", "end-1c")
+        textbox_comp.delete("1.0", "end-1c")
+
+    def menu_consume_fraud():
+        consume_fraud_label = ['User(用户):', 'UID(用户ID)', 'Date(日期):', 'Address(地址):', 'Currency(货币):',
+                               'City,Zip-code(城市,邮编):', 'Laws of the State(管辖权):']
+        i = 0
+        for con in consume_fraud_label:
+            Label(text=con).grid(column=0, row=i)
+            i += 1
 
     win = Tk()
-    win.title('WeProtect内部系统 Inner Contract Gen System')
+    win.title('WeProtect内部合同生成系统/Contract Gen System')
     win.geometry("800x300")
+
+    #  创建菜单栏 在窗口的上方
+    menubar = Menu(win)
+
+    #  创建新菜单栏
+    menu_file = Menu(menubar, tearoff=0)
+    menu_personal_law = Menu(menubar, tearoff=0)
+    menu_business_law = Menu(menubar, tearoff=0)
+
+    # 命名空菜单，放进菜单栏中
+    menubar.add_cascade(label='File', menu=menu_file)
+    menubar.add_cascade(label='Personal_Law', menu=menu_personal_law)
+    menubar.add_cascade(label='Business_Law', menu=menu_business_law)
+
+    # 在File菜单中，添加New\open\exit等小菜单，每一个小菜单对应命令操作
+    menu_file.add_command(label='New', command=menu_New)
+    menu_file.add_command(label='Quit', command=win.quit)
+
+    # 个人法律菜单栏的业务作为小菜单，每个小菜单对应一个新的windows，里面的要填的内容需要随时更改
+    personal_law_product = ['消费纠纷', '民事纠纷', '移民签证', '劳务纠纷', '交通罚单', '刑事案件', '法律文件撰写', '小额法庭',
+                            '合同审核']
+    for p in personal_law_product:
+        menu_personal_law.add_command(label=p, command=menu_consume_fraud)  # todo 每个个人业务要有一个新command 改变填写内容
+
+    # 商业法律菜单栏的业务作为小菜单，每个小菜单对应一个新的windows，里面的要填的内容需要随时更改
+    business_law_product = ['知识产权保护', '海关事务', '产品合规', '商业合同', '跨境专项服务']
+    for bus in business_law_product:
+        menu_business_law.add_command(label=bus, command=menu_consume_fraud)  # todo 每个商业业务要有一个新command 改变填写内容
+
+    # 配置 显示菜单栏menubar
+    win.config(menu=menubar)
 
     #  grid label
     label_name = ['User(用户):', 'UID(用户ID)', 'Date(日期):', 'Address(地址):', 'Currency(货币):', 'City,Zip-code(城市,邮编):',
                   'Laws of the State(管辖权):', 'Effectiveness(时效(月份)):', 'Days of Notice Ahead(提前几日通知):',
                   'Compensation(酬劳条款):']
 
+    textbox_var = ['name', 'uid', 'date', 'addr', 'curr', 'city', 'laws', 'effe', 'noti', 'comp']
+
+    # 生成label名字
     i = 0
-    for c in label_name:
-        Label(text=c).grid(column=0, row=i)
+    for a in label_name:
+        Label(text=a).grid(column=0, row=i)
         i = i + 1
 
+    # 生成对应的textbox
     textbox_name = Text(win, height=1)
-    textbox_name.grid(column=1, row=0)
-    textbox_name.bind("<Tab>", focus_next_window)
     textbox_uid = Text(win, height=1)
-    textbox_uid.grid(column=1, row=1)
-    textbox_uid.bind("<Tab>", focus_next_window)
     textbox_date = Text(win, height=1)
-    textbox_date.grid(column=1, row=2)
-    textbox_date.bind("<Tab>", focus_next_window)
     textbox_addr = Text(win, height=1)
-    textbox_addr.grid(column=1, row=3)
-    textbox_addr.bind("<Tab>", focus_next_window)
     textbox_curr = Text(win, height=1)
-    textbox_curr.grid(column=1, row=4)
-    textbox_curr.bind("<Tab>", focus_next_window)
     textbox_city = Text(win, height=1)
-    textbox_city.grid(column=1, row=5)
-    textbox_city.bind("<Tab>", focus_next_window)
     textbox_laws = Text(win, height=1)
-    textbox_laws.grid(column=1, row=6)
-    textbox_laws.bind("<Tab>", focus_next_window)
     textbox_effe = Text(win, height=1)
-    textbox_effe.grid(column=1, row=7)
-    textbox_effe.bind("<Tab>", focus_next_window)
     textbox_noti = Text(win, height=1)
-    textbox_noti.grid(column=1, row=8)
-    textbox_noti.bind("<Tab>", focus_next_window)
     textbox_comp = Text(win, height=3)
-    textbox_comp.grid(column=1, row=9)
-    textbox_comp.bind("<Tab>", focus_next_window)
+
+    x = 0
+    for b in textbox_var:
+        exec('textbox_{}.grid(column=1, row={})'.format(b, x))
+        exec('textbox_{}.bind("<Tab>", focus_next_window)'.format(b))
+        x = x + 1
 
     old_date = 'AIdate'
     old_name = 'AIname'
@@ -169,7 +208,7 @@ def main():
 
     #  word生成 按钮
     # Button(win, text='合同生成', command=word_gen).place(x=90, y=380)
-    Button(win, text='合同生成(Gen Contract)', command=word_gen).grid(column=0, row=14)
+    Button(win, text='合同生成(Gen Contract)', command=word_gen).grid(column=1, row=14)
     win.iconbitmap('./logo.ico')
     win.mainloop()  # 进入消息循环
 
